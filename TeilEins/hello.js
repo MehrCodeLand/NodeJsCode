b3cae5536bc0f8dbdfb5b964408d802486693d00
MehrCodeLand <mehrcodeland@gmail.com>
2023-01-08 18:01:57 +0330

    hello world and js

---
+++ TeilEins/hello.js
@@ -0,0 +1,20 @@
+// 3 way to create function 
+
+// 1 
+function Sum(number1 , number2 ){
+    return number1 + number2;
+}
+
+// 2 
+const SumNumber = function(number1 , number2 ){
+    return number1 + number2;
+}
+
+// 3-1 *arrow function
+const SumNumbers = (number1 , number2) => {
+    return number1 + number2;
+}
+// 3-2 *arrow function
+const SumNum = (number1 , number2) => number1 + number2;
+// 3-3 *arrow function
+const SumNumbers1 = number1 => number1 + 100 ;
